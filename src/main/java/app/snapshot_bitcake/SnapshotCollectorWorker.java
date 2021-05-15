e43ac75ab4eb5708d2017554d7ac7bc6987a4243
Boris Jankovic <boris@tenderly.co>
2021-05-15 23:05:44 +0200

    Init project

---
+++ src/main/java/app/snapshot_bitcake/SnapshotCollectorWorker.java
@@ -0,0 +1,162 @@
+package app.snapshot_bitcake;
+
+import app.AppConfig;
+import servent.message.Message;
+import servent.message.snapshot.NaiveAskAmountMessage;
+import servent.message.util.MessageUtil;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Main snapshot collector class. Has support for Naive, Chandy-Lamport
+ * and Lai-Yang snapshot algorithms.
+ * 
+ * @author bmilojkovic
+ *
+ */
+public class SnapshotCollectorWorker implements SnapshotCollector {
+
+	private volatile boolean working = true;
+	
+	private final AtomicBoolean collecting = new AtomicBoolean(false);
+	
+	private final Map<String, Integer> collectedNaiveValues = new ConcurrentHashMap<>();
+
+	private SnapshotType snapshotType = SnapshotType.NAIVE;
+	
+	private BitcakeManager bitcakeManager;
+
+	public SnapshotCollectorWorker(SnapshotType snapshotType) {
+		this.snapshotType = snapshotType;
+
+		switch (snapshotType) {
+			case NAIVE -> bitcakeManager = new NaiveBitcakeManager();
+			case NONE -> {
+				AppConfig.timestampedErrorPrint("Making snapshot collector without specifying type. Exiting...");
+				System.exit(0);
+			}
+		}
+	}
+	
+	@Override
+	public BitcakeManager getBitcakeManager() {
+		return bitcakeManager;
+	}
+	
+	@Override
+	public void run() {
+		while(working) {
+			
+			/*
+			 * Not collecting yet - just sleep until we start actual work, or finish
+			 */
+			while (!collecting.get()) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				if (!working) {
+					return;
+				}
+			}
+			
+			/*
+			 * Collecting is done in three stages:
+			 * 1. Send messages asking for values
+			 * 2. Wait for all the responses
+			 * 3. Print result
+			 */
+			
+			//1 send asks
+			switch (snapshotType) {
+			case NAIVE:
+				Message askMessage = new NaiveAskAmountMessage(AppConfig.myServentInfo, null);
+				
+				for (Integer neighbor : AppConfig.myServentInfo.getNeighbors()) {
+					askMessage = askMessage.changeReceiver(neighbor);
+					
+					MessageUtil.sendMessage(askMessage);
+				}
+				collectedNaiveValues.put("node"+AppConfig.myServentInfo.getId(), bitcakeManager.getCurrentBitcakeAmount());
+				break;
+			case NONE:
+				//Shouldn't be able to come here. See constructor. 
+				break;
+			}
+			
+			//2 wait for responses or finish
+			boolean waiting = true;
+			while (waiting) {
+				switch (snapshotType) {
+				case NAIVE:
+					if (collectedNaiveValues.size() == AppConfig.getServentCount()) {
+						waiting = false;
+					}
+					break;
+				case NONE:
+					//Shouldn't be able to come here. See constructor. 
+					break;
+				}
+				
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+				if (!working) {
+					return;
+				}
+			}
+			
+			//print
+			int sum;
+			switch (snapshotType) {
+			case NAIVE:
+				sum = 0;
+				for (Entry<String, Integer> itemAmount : collectedNaiveValues.entrySet()) {
+					sum += itemAmount.getValue();
+					AppConfig.timestampedStandardPrint(
+							"Info for " + itemAmount.getKey() + " = " + itemAmount.getValue() + " bitcake");
+				}
+				
+				AppConfig.timestampedStandardPrint("System bitcake count: " + sum);
+				
+				collectedNaiveValues.clear(); //reset for next invocation
+				break;
+			case NONE:
+				//Shouldn't be able to come here. See constructor. 
+				break;
+			}
+			collecting.set(false);
+		}
+
+	}
+	
+	@Override
+	public void addNaiveSnapshotInfo(String snapshotSubject, int amount) {
+		collectedNaiveValues.put(snapshotSubject, amount);
+	}
+
+	@Override
+	public void startCollecting() {
+		boolean oldValue = this.collecting.getAndSet(true);
+		
+		if (oldValue) {
+			AppConfig.timestampedErrorPrint("Tried to start collecting before finished with previous.");
+		}
+	}
+	
+	@Override
+	public void stop() {
+		working = false;
+	}
+
+}
---
+++ src/main/java/servent/SimpleServentListener.java
@@ -0,0 +1,95 @@
+package servent;
+
+import app.AppConfig;
+import app.Cancellable;
+import app.snapshot_bitcake.SnapshotCollector;
+import servent.handler.*;
+import servent.handler.snapshot.*;
+import servent.message.Message;
+import servent.message.util.MessageUtil;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SimpleServentListener implements Runnable, Cancellable {
+
+	private volatile boolean working = true;
+	
+	private final SnapshotCollector snapshotCollector;
+	
+	public SimpleServentListener(SnapshotCollector snapshotCollector) {
+		this.snapshotCollector = snapshotCollector;
+	}
+
+	/*
+	 * Thread pool for executing the handlers. Each client will get it's own handler thread.
+	 */
+	private final ExecutorService threadPool = Executors.newWorkStealingPool();
+	
+	@Override
+	public void run() {
+		ServerSocket listenerSocket = null;
+		try {
+			listenerSocket = new ServerSocket(AppConfig.myServentInfo.getListenerPort(), 100);
+			/*
+			 * If there is no connection after 1s, wake up and see if we should terminate.
+			 */
+			listenerSocket.setSoTimeout(1000);
+		} catch (IOException e) {
+			AppConfig.timestampedErrorPrint("Couldn't open listener socket on: " + AppConfig.myServentInfo.getListenerPort());
+			System.exit(0);
+		}
+		
+		while (working) {
+			try {
+				Message clientMessage;
+				
+				/*
+				 * This blocks for up to 1s, after which SocketTimeoutException is thrown.
+				 */
+				Socket clientSocket = listenerSocket.accept();
+
+				//GOT A MESSAGE! <3
+				clientMessage = MessageUtil.readMessage(clientSocket);
+
+				MessageHandler messageHandler = new NullHandler(clientMessage);
+				
+				/*
+				 * Each message type has it's own handler.
+				 * If we can get away with stateless handlers, we will,
+				 * because that way is much simpler and less error prone.
+				 */
+				switch (clientMessage.getMessageType()) {
+				case TRANSACTION:
+					messageHandler = new TransactionHandler(clientMessage, snapshotCollector.getBitcakeManager());
+					break;
+				case NAIVE_ASK_AMOUNT:
+					messageHandler = new NaiveAskAmountHandler(clientMessage, snapshotCollector.getBitcakeManager());
+					break;
+				case NAIVE_TELL_AMOUNT:
+					messageHandler = new NaiveTellAmountHandler(clientMessage, snapshotCollector);
+					break;
+				case POISON:
+					break;
+				}
+				
+				threadPool.submit(messageHandler);
+			} catch (SocketTimeoutException timeoutEx) {
+				//Uncomment the next line to see that we are waking up every second.
+//				AppConfig.timedStandardPrint("Waiting...");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void stop() {
+		this.working = false;
+	}
+
+}
